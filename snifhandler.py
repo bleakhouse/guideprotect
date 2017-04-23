e4c68d5795b894b464c9f97b64e9cd205c3310e6
kkindof <surewin.chau@hotmail.com>
2017-04-24 00:27:40 +0800

    no need to import

--- snifhandler.py
+++ snifhandler.py
@@ -18,21 +18,7 @@ from scapy.all import *
 import gpconf
 import basedef
 
-from BaseHTTPServer import BaseHTTPRequestHandler
-from StringIO import StringIO
-
 g_redirect_count = 0
-
-class HTTPRequest(BaseHTTPRequestHandler):
-    def __init__(self, request_text):
-        self.rfile = StringIO(request_text)
-        self.raw_requestline = self.rfile.readline()
-        self.error_code = self.error_message = None
-        self.parse_request()
-
-    def send_error(self, code, message):
-        self.error_code = code
-        self.error_message = message
 
 def inject_back_url(pkt, newtarget):
 
@@ -108,3 +94,4 @@ def sniff_check_http_packet(pkt):
         return
 
     inject_back_url(pkt, redirect_info)
+
