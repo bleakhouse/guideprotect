cc83378618a8b68d3bc75d4cf5313bd99736329c
kkindof <surewin.chau@hotmail.com>
2017-06-16 09:32:58 +0800

    fix deployment

--- deployment.sh
+++ deployment.sh
@@ -1,11 +1,13 @@
 if [ ! -d ./guideprotect ]; then
     git clone https://github.com/bleakhouse/guideprotect
 fi
-
+apt-get install -y redis-server
+systemctl enable mysql
+systemctl start mysql
 pip install netifaces
 pip install scapy
 pip install web.py
-pip install redis-server
+pip install redis
 mysql -u root -e "grant all privileges on *.* to test@localhost identified by '123456';"
 mysql -u root -e "grant all privileges on *.* to test@127.0.0.1 identified by '123456';"
 mysql -u root -e "flush privileges;"
