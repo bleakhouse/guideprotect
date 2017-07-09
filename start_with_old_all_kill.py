3d1fd4545e6c385eca74c694cc11e098b9a2536e
kkindof <surewin.chau@hotmail.com>
2017-07-09 18:35:25 +0800

    make a new starter

--- start_with_old_all_kill.py
+++ start_with_old_all_kill.py
@@ -5,6 +5,7 @@ import json
 
 import mylogging
 import os
+import save_log_redis
 cmdlines = [
     '''pkill -f 'python save_log_redis.py' ''',
     '''pkill -f 'python guideprotect.py' ''',
@@ -12,6 +13,11 @@ cmdlines = [
     'python guideprotect.py'
 
             ]
+
+obj = save_log_redis.SaveLogging2Redis()
+obj.init()
+obj.save2pub({'_dtype':999})
+
 for cmdline in cmdlines:
     os.system(cmdline)
 
