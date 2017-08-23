11bbfcf5a7d5cf69b839f699c071dbc8bef3900a
kkindof <5771067@qq.com>
2017-08-23 12:50:54 +0800

    install pip install psutil

--- deployment.sh
+++ deployment.sh
@@ -8,6 +8,7 @@ pip install netifaces
 pip install scapy
 pip install web.py
 pip install redis
+pip install psutil
 mysql -u root -e "grant all privileges on *.* to test@localhost identified by '123456';"
 mysql -u root -e "grant all privileges on *.* to test@127.0.0.1 identified by '123456';"
 mysql -u root -e "flush privileges;"
