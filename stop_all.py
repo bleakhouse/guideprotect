400df60e018f1e5df34616535c0af301d3e12801
kkindof <5771067@qq.com>
2017-11-16 21:25:36 +0800

    fix stop all

--- stop_all.py
+++ stop_all.py
@@ -8,7 +8,7 @@ import os
 cmdlines = [
     '''pkill -f 'python save_log_redis.py' ''',
     '''pkill -f 'python guideprotect.py' ''',
-    '''pkill -f 'python new_url_updator.py' '''
+    '''pkill -f 'python new_url_updator.py 8787' '''
     '''pkill -f 'python check404.py' '''
     '''pkill -f 'python gp_mq_puller.py zmqp1' ''',
     '''pkill -f 'python guideprotect.py' '''
